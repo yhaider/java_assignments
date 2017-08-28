4e9d069756f7f0c21491d9ee27650c0e2e256f26
Yasmeen <yhaider@uchicago.edu>
2017-08-28 12:18:17 -0400

    initial commit

---
+++ java_fundamentals/FirstJavaProgram.java
@@ -0,0 +1,10 @@
+// Create a class that will print your name, age, and hometown
+// on separate lines
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.printIn("My name is Yasmeen.");
+        System.out.printIn("I am 20 years old.");
+        System.out.printIn("My hometown is Potomac, MD.");
+    }
+}
