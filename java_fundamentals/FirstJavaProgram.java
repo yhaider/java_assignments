5a6bbf8f248bd56c6565f09b9994a037bf74ebe0
Yasmeen <yhaider@uchicago.edu>
2017-08-28 12:22:29 -0400

    first assgnt fix

--- java_fundamentals/FirstJavaProgram.java
+++ java_fundamentals/FirstJavaProgram.java
@@ -1,10 +1,10 @@
 // Create a class that will print your name, age, and hometown
 // on separate lines
 
-public class FirstJavaProgram {
-    public static void main(String[] args) {
-        System.out.printIn("My name is Yasmeen.");
-        System.out.printIn("I am 20 years old.");
-        System.out.printIn("My hometown is Potomac, MD.");
+public class FirstJavaProgram{
+    public static void main(String[] args){
+        System.out.println("My name is Yasmeen.");
+        System.out.println("I am 20 years old.");
+        System.out.println("My hometown is Potomac, MD.");
     }
 }
