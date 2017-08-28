3bb1b3a4b92cf56147abb9062eb100839a534f4a
Yasmeen <yhaider@uchicago.edu>
2017-08-28 13:12:36 -0400

    added fizzbuzz

---
+++ java_fundamentals/fizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,8 @@
+
+public class FizzBuzzTest{
+    public static void main(String[] args){
+        FizzBuzz fb = new FizzBuzz();
+        String result = fb.fizzBuzz(15);
+        System.out.println(result); // should return FizzBuzz.
+    }
+}
