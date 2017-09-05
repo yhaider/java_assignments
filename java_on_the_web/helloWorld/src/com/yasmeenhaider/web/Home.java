0f4808d3dcf3089d5c0d2868261263902aa8ea84
Yasmeen <yhaider@uchicago.edu>
2017-09-05 12:23:04 -0400

    added helloworld wip

---
+++ java_on_the_web/helloWorld/src/com/yasmeenhaider/web/Home.java
@@ -0,0 +1,43 @@
+package com.yasmeenhaider.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/Home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Sets the content type of the response
+        response.setContentType("text/html");
+        // writes the response
+        PrintWriter out = response.getWriter();
+        out.write("<h1>Hello World</h1>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
